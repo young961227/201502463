97fb6b54bd126b049b1bf6828f2b8773c0269d28
young sang lee <yohesa@nate.com>
2020-05-08 18:25:15 +0900

    test

--- poker.py
+++ poker.py
@@ -3,12 +3,9 @@ from abc import ABCMeta, abstractmethod
 suits = 'CDHS'
 ranks = '23456789TJQKA'
 
-from abc import ABCMeta, abstractmethod
-
 class Card(metaclass=ABCMeta):
     """Abstact class for playing cards
     """
-    @abstractmethod
     def __init__(self, rank_suit):
         if rank_suit[0] not in ranks or rank_suit[1] not in suits:
             raise ValueError(f'{rank_suit}: illegal card')
